4123bd161d67fe3a080e1bd803f016ec1776f869
Justin Dray <justin@dray.be>
2013-06-16 19:17:24 +1000

    Updated for new find version

--- cleanup-torrents.sh
+++ cleanup-torrents.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /tmp/torrents
-find /raid/shares/public -iname .added -exec mv {} /tmp/torrents \;
+find /raid/shares/public -iname "*.added" -exec mv {} /tmp/torrents \;
 exit 0
