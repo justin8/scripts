c0603ff0e4f3de23aed4a32f81232f0b023e4cad
Justin Dray <justin@dray.be>
2013-10-09 18:27:38 +1000

    Changed to new update url for IP address

--- updateip.sh
+++ updateip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --retry 400 http://ipv4.cloudns.net/api/dynamicURL/?q=NzU4MTE6NDg4ODUyOmEzZThjOTA0OWE2OTBhMDJmYjk2NzA3Y2M3MTQ5MzU1MWY1OWUwOTRkMWY4NTUwMGNjMWM3YWNhYzA5ZWFhZmQ > /dev/null
+curl --retry 400 http://ipv4.cloudns.net/api/dynamicURL/?q=NzU4MTE6MzE0NzEwNjozZTkxZGUwMDg1MjAwMTM5YTg5MDI4NDE2NzY4OTE1Nzk4ZjcxM2U1NTg3ZDM1MTJmM2RlYTdjNWZhZTgyZDY5 > /dev/null 2>&1
