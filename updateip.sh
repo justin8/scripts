2dbab6707771f0361ec460f525f754a975d909f6
Justin Dray <justin@dray.be>
2013-11-09 12:50:15 +1000

    Changed to new URL for updateip script

--- updateip.sh
+++ updateip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --retry 400 http://ipv4.cloudns.net/api/dynamicURL/?q=NzU4MTE6MzE0NzEwNjozZTkxZGUwMDg1MjAwMTM5YTg5MDI4NDE2NzY4OTE1Nzk4ZjcxM2U1NTg3ZDM1MTJmM2RlYTdjNWZhZTgyZDY5 > /dev/null 2>&1
+curl --retry 400 http://ipv4.cloudns.net/api/dynamicURL/?q=NzU4MTE6MzI1ODcxNzo0MWE1NmRiZTFkMmY2ODc1ZmVjZjdiZDkxOGM5ZDc3M2JmOTkxNWUxOGE3ODNkYTc4ZWFmZDJhNTMzOTQ2MzFk > /dev/null 2>&1
