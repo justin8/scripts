7e696e1a97ab9c1fd2679d08916105c338506412
Justin Dray <justin@dray.be>
2014-03-17 11:12:36 +1000

    Added check for python27

--- vmware_to_racktables/rh6_run_vmware_to_racktables.sh
+++ vmware_to_racktables/rh6_run_vmware_to_racktables.sh
@@ -15,6 +15,12 @@ exitfunc()
 exec > $LOG
 trap exitfunc EXIT
 
+if [[ ! -d /opt/rh/python27 ]]
+then
+	echo "vmware_to_racktables requires python27 to be installed!"
+	exit 1
+fi
+
 source /opt/rh/python27/enable
 cd "$(dirname "$(readlink -f "$0")")"
 /root/.virtualenvs/vmware_to_racktables/bin/python2 vmware_to_racktables.py -vv
