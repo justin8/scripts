ec4f52fde65cdbb1950f85271837d33a5feacab3
Justin Dray <justin@dray.be>
2014-04-03 20:30:47 +1000

    encode.sh: Added error checking

--- encode.sh
+++ encode.sh
@@ -6,5 +6,12 @@ do
 	outfile=$(basename "$infile")
 	outfile=${outfile%.*}
 	ffmpeg -i "$infile" -vcodec libx264 -crf 23 -preset veryslow -acodec libfdk_aac -ab 128k -ac 2 "$dir/complete/${outfile}.mkv"
-	[[ $? == 0 ]] && rm "$infile"
+	rc=$?
+	if [[ $rc == 0 ]]
+	then
+		echo -e "\e[32;1mCompleted encoding $infile\e[0m\n\n\n"
+		rm "$infile"
+	else
+		echo -e "\e[31;1mFailed to encode $infile. (RC=$rc)\e[0m\n\n\n"
+	fi
 done
