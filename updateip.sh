714ff3e6410cc4cd23009d2eb63ab9ef9c6c4467
Justin Dray <justin@dray.be>
2014-07-15 00:41:24 +1000

    Updated updateip to hopefully get some debugging info out of it

--- updateip.sh
+++ updateip.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-curl --retry 400 http://ipv4.cloudns.net/api/dynamicURL/?q=NzU4MTE6MzI1ODcxNzo0MWE1NmRiZTFkMmY2ODc1ZmVjZjdiZDkxOGM5ZDc3M2JmOTkxNWUxOGE3ODNkYTc4ZWFmZDJhNTMzOTQ2MzFk > /dev/null 2>&1
+LOG="/var/log/$(basename "$(readlink -f "$0")")"
+
+exec >> $LOG 2>&1
+
+echo -n "$(date +'%Y-%m%d %H:%M:%S'): "
+curl -Ss --retry 400 "http://ipv4.cloudns.net/api/dynamicURL/?q=NzU4MTE6MzI1ODcxNzo0MWE1NmRiZTFkMmY2ODc1ZmVjZjdiZDkxOGM5ZDc3M2JmOTkxNWUxOGE3ODNkYTc4ZWFmZDJhNTMzOTQ2MzFk"
+echo
