243ce3ce2ce975a4be2d81ee5ecb3dc6217fae94
Justin Dray <justin@dray.be>
2015-06-11 13:20:51 +1000

    Fixed file extension handling

--- encode.sh
+++ encode.sh
@@ -28,7 +28,7 @@ if flock -xn 200
 then
 	for infile in $indir/*
 	do
-		outfile=$(basename "$infile")
+		outfile="$(basename "$infile")"
 		outfile="${outfile%.*}.mkv"
 
 		if [[ -f "$outdir/$outfile" ]]
@@ -36,13 +36,13 @@ then
 			echo "$infile has already been processed!"
 		else
 
-			ffmpeg -i "$infile" -vcodec libx264 -crf 23 -preset veryslow -acodec libfdk_aac -ab 128k -ac 2 "$tempdir/${outfile}.mkv"
+			ffmpeg -i "$infile" -vcodec libx264 -crf 20 -preset veryslow -acodec libfdk_aac -ab 128k -ac 2 "$tempdir/${outfile}"
 			rc=$?
 			if [[ $rc == 0 ]]
 			then
 				echo -e "\e[32;1mCompleted encoding $infile\e[0m\n\n\n"
 				rm "$infile"
-				mv "$tempdir/${outfile}.mkv" "$outdir"
+				mv "$tempdir/${outfile}" "$outdir"
 			else
 				echo -e "\e[31;1mFailed to encode $infile. (RC=$rc)\e[0m\n\n\n"
 				mv "$infile" "$dir/failed"
