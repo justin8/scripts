012128ede6b68d1b3ed114debc975628c17179ec
Justin Dray <justin@dray.be>
2015-11-12 21:42:19 +0100

    Fix for latest ffmpeg version

--- ffmpeg-justin-patcher.sh
+++ ffmpeg-justin-patcher.sh
@@ -3,7 +3,7 @@ set -e
 sed -i 's/pkgname=.*/pkgname=ffmpeg-justin\n_pkgname=ffmpeg/' PKGBUILD
 sed -i "s/depends=(/depends=('libaacplus' 'faac' 'libfdk-aac' 'libsoxr' 'vo-aacenc' 'vo-amrwbenc' /" PKGBUILD
 sed -i 's/{,\.asc}//g' PKGBUILD
-sed -i '/md5sums/s/$/)/' PKGBUILD
+#sed -i '/md5sums/s/$/)/' PKGBUILD
 sed -i '/SKIP/d' PKGBUILD
 sed -i 's/$pkgname/$_pkgname/' PKGBUILD
 sed -i 's/epoch=/conflicts=ffmpeg\nepoch=/' PKGBUILD
