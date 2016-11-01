8ed195bc4afa33efc8a55ef64091ae151fd74a99
Justin Dray <justin@dray.be>
2016-11-02 08:43:04 +1000

    Remove libfaac

--- ffmpeg-justin-patcher.sh
+++ ffmpeg-justin-patcher.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 sed -i 's/pkgname=.*/pkgname=ffmpeg-justin\n_pkgname=ffmpeg/' PKGBUILD
-sed -i "s/depends=(/depends=('faac' 'libfdk-aac' 'libsoxr' /" PKGBUILD
+sed -i "s/depends=(/depends=('libfdk-aac' 'libsoxr' /" PKGBUILD
 sed -i 's/{,\.asc}//g' PKGBUILD
 sed -i 's/${pkgname}/${_pkgname}/' PKGBUILD
 sed -i 's/epoch=/conflicts=("ffmpeg")\nepoch=/' PKGBUILD
 sed -i "s/provides=(/provides=('ffmpeg' /" PKGBUILD
-sed -i 's#./configure #./configure --enable-libfaac --enable-libfdk-aac --enable-libsoxr --enable-pic --enable-postproc --enable-nonfree --enable-opengl --enable-openssl #' PKGBUILD
+sed -i 's#./configure #./configure --enable-libfdk-aac --enable-libsoxr --enable-pic --enable-postproc --enable-nonfree --enable-opengl --enable-openssl #' PKGBUILD
 
 updpkgsums
