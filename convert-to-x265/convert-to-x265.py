99ddd486272b05ab8ef6803777aed8a3f7ae3f94
Justin Dray <justin@dray.be>
2016-11-06 14:54:41 +1000

    Change to 20 quality

--- convert-to-x265/convert-to-x265.py
+++ convert-to-x265/convert-to-x265.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
                         action="store")
     parser.add_argument("-q", "--quality",
                         help="Quality quantizer",
-                        default=21,
+                        default=20,
                         action="store")
     parser.add_argument("-p", "--preset",
                         help="Encoding preset to use",
